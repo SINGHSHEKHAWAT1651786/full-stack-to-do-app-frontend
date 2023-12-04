0c804c7618cee36c4fc13da9a44a3e372f5fedf9
SINGHSHEKHAWAT1651786 <128773189+SINGHSHEKHAWAT1651786@users.noreply.github.com>
2023-12-04 22:40:39 +0530

    Add files via upload

---
+++ controllers/ToDoController.js
@@ -0,0 +1,48 @@
+const ToDoModel = require('../models/ToDoModel');
+
+module.exports.getToDo = async (req, res) => {
+    try {
+        const toDo = await ToDoModel.find();
+        res.send(toDo);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+module.exports.saveToDo = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const data = await ToDoModel.create({ text });
+        console.log("Added Successfully...");
+        console.log(data);
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+module.exports.updateToDo = async (req, res) => {
+    try {
+        const { _id, text } = req.body;
+        await ToDoModel.findByIdAndUpdate(_id, { text });
+        res.send("Updated Successfully...");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+module.exports.deleteToDo = async (req, res) => {
+    try {
+        const { _id } = req.body;
+        await ToDoModel.findByIdAndDelete(_id);
+        res.send("Deleted Successfully...");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+
